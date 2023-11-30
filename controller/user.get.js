b47e44b22508674e28b89918ae86b2ffaef7af71
Mateo Blanco <mateo997@outlook.com>
2023-11-30 09:50:08 -0500

    get users

--- controller/user.get.js
+++ controller/user.get.js
@@ -1,5 +1,15 @@
-const getUsers = async (req,res) => {
+const Usuario = require ('../model/user');
 
+const getUsers = async (req,res) => {
+  let usrSh =[];
+  Usuario.find().then((users)=>{
+    users.forEach(u=>{
+      let {__v, Password, ...resto} = u._doc;
+      usrSh.push(resto)
+    });
+    
+    res.json(usrSh);
+  });
 }
 
 module.exports = getUsers;
